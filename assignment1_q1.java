c33cd6b70beb0640cf69b9459c017a24cab1a05d
codersauravv <91203619+codersauravv@users.noreply.github.com>
2021-09-28 16:46:15 +0530

    file uploaded

---
+++ assignment1_q1.java
@@ -0,0 +1,9 @@
+// q1.write a Java program to print 'Hello' on screen and then print your name on a separate line.
+package assignment1;
+
+public class assignment1_q1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
